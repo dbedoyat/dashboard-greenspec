2b7652a17db9f95f71d878f1c1cca4b5b6f9758f
David Bedoya <dbedoyat@gmail.com>
2025-01-02 23:42:54 -0500

    lottie

--- src/components/Laptop/index.tsx
+++ src/components/Laptop/index.tsx
@@ -2,9 +2,11 @@
 import React from 'react';
 import {DotLottieReact} from '@lottiefiles/dotlottie-react';
 
+import styles from './Laptop.module.css';
+
 const Laptop = () => {
   return (
-    <div>
+    <div className={styles.laptop}>
       <DotLottieReact src='/laptop.lottie' loop autoplay />{' '}
     </div>
   );
